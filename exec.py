850f88d27856db1efdafdc0f16f27b56bd84d2ec
amitrajaraman <amitrajaraman@cse.iitb.ac.in>
2020-09-21 14:17:10 +0530

    added terminal option

--- exec.py
+++ exec.py
@@ -3,5 +3,13 @@ from bot import T_bot
 #from bot import *
 
 if __name__ == "__main__":
-	g = T_bot()
+	choice = input('Enter 0 to play Tetris and 1 to give us that satisfaction: ')
+	if (choice == '0'):
+		g = tetris()
+	elif (choice == '1'):
+		g = T_bot()
+	else:
+		print('All you had to do was enter the damn choice!')
+		print('Terminating')
+		quit()
 	g.run()
